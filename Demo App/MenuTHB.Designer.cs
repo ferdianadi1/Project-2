2a919ad28a2b962c97a2141b9e5b445333d003ea
Unknown <ferdianadi1@gmail.com>
2018-04-10 14:16:32 +0700

    Penambahan Label Stasiun awal

--- Demo App/MenuTHB.Designer.cs
+++ Demo App/MenuTHB.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.btn_refund = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_refund
@@ -41,20 +43,43 @@
             this.btn_refund.UseVisualStyleBackColor = true;
             this.btn_refund.Click += new System.EventHandler(this.btn_refund_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(162, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Silahkan Pilih Stasiun Awal Anda";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Jakarta";
+            // 
             // MenuTHB
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(637, 331);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btn_refund);
             this.Name = "MenuTHB";
             this.Text = "MenuTHB";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_refund;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
